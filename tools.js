263de0ebe9900f0c89b641885c914cc654e1da97
Yash Mehta <yashmehta0395@gmail.com>
2023-09-27 20:56:45 +0530

    tools functionality added

---
+++ tools.js
@@ -0,0 +1,29 @@
+let toolsCont = document.querySelector('.tools-cont');
+let optionsCont = document.querySelector('.options-cont');
+let pencilToolCont = document.querySelector('.pencil-tool-cont');
+let eraserToolCont = document.querySelector('.eraser-tool-cont');
+let optionsFlag = true;
+
+// true -> tools show, false -> hide tools
+optionsCont.addEventListener("click", (e) => {
+    optionsFlag = !optionsFlag;
+
+    if (optionsFlag) openTools();
+    else closeTools();
+})
+
+function openTools() {
+    let iconElem = optionsCont.children[0];
+    iconElem.classList.remove("fa-times");
+    iconElem.classList.add("fa-bars");
+    toolsCont.style.display = "flex";
+}
+function closeTools() {
+    let iconElem = optionsCont.children[0];
+    iconElem.classList.remove("fa-bars");
+    iconElem.classList.add("fa-times");
+    toolsCont.style.display = "none";
+    
+    pencilToolCont.style.display = "none";
+    eraserToolCont.style.display = "none";
+}
