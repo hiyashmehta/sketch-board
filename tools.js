fad4b7fe3904cee43fb07d955169a2030f5728a8
Yash Mehta <yashmehta0395@gmail.com>
2023-09-27 21:45:38 +0530

    added open/close function for pencil and eraser

--- tools.js
+++ tools.js
@@ -1,8 +1,12 @@
 let toolsCont = document.querySelector('.tools-cont');
 let optionsCont = document.querySelector('.options-cont');
+let optionsFlag = true;
 let pencilToolCont = document.querySelector('.pencil-tool-cont');
 let eraserToolCont = document.querySelector('.eraser-tool-cont');
-let optionsFlag = true;
+let pencil = document.querySelector('.pencil');
+let eraser = document.querySelector('.eraser');
+let pencilFlag = false;
+let eraserFlag = false;
 
 // true -> tools show, false -> hide tools
 optionsCont.addEventListener("click", (e) => {
@@ -23,7 +27,22 @@ function closeTools() {
     iconElem.classList.remove("fa-bars");
     iconElem.classList.add("fa-times");
     toolsCont.style.display = "none";
-    
+
     pencilToolCont.style.display = "none";
     eraserToolCont.style.display = "none";
 }
+
+pencil.addEventListener("click", (e) => {
+    // true -> show pencil tool, false -> hide pencil tool
+    pencilFlag = !pencilFlag;
+
+    if (pencilFlag) pencilToolCont.style.display = "block";
+    else pencilToolCont.style.display = "none";
+})
+eraser.addEventListener("click", (e) => {
+    // true -> show eraser tool, false -> hide eraser tool
+    eraserFlag = !eraserFlag;
+
+    if (eraserFlag) eraserToolCont.style.display = "flex";
+    else eraserToolCont.style.display = "none";
+})
