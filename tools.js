9fd3b2b904e9b8828b9796d2dc502950cf2b3ecf
Yash Mehta <yashmehta0395@gmail.com>
2023-09-28 13:18:14 +0530

    added canvas to the project

--- tools.js
+++ tools.js
@@ -1,22 +1,23 @@
-let toolsCont = document.querySelector('.tools-cont');
-let optionsCont = document.querySelector('.options-cont');
+let toolsCont = document.querySelector(".tools-cont");
+let optionsCont = document.querySelector(".options-cont");
 let optionsFlag = true;
-let pencilToolCont = document.querySelector('.pencil-tool-cont');
-let eraserToolCont = document.querySelector('.eraser-tool-cont');
-let pencil = document.querySelector('.pencil');
-let eraser = document.querySelector('.eraser');
-let sticky = document.querySelector('.sticky');
-let upload = document.querySelector('.upload');
+let pencilToolCont = document.querySelector(".pencil-tool-cont");
+let eraserToolCont = document.querySelector(".eraser-tool-cont");
+let pencil = document.querySelector(".pencil");
+let eraser = document.querySelector(".eraser");
+let sticky = document.querySelector(".sticky");
+let upload = document.querySelector(".upload");
 let pencilFlag = false;
 let eraserFlag = false;
 
-// true -> tools show, false -> hide tools
 optionsCont.addEventListener("click", (e) => {
+    // true -> tools show, false -> hide tools
     optionsFlag = !optionsFlag;
 
     if (optionsFlag) openTools();
     else closeTools();
 })
+
 
 function openTools() {
     let iconElem = optionsCont.children[0];
@@ -41,6 +42,7 @@ pencil.addEventListener("click", (e) => {
     if (pencilFlag) pencilToolCont.style.display = "block";
     else pencilToolCont.style.display = "none";
 })
+
 eraser.addEventListener("click", (e) => {
     // true -> show eraser tool, false -> hide eraser tool
     eraserFlag = !eraserFlag;
@@ -50,29 +52,29 @@ eraser.addEventListener("click", (e) => {
 })
 
 upload.addEventListener("click", (e) => {
-      // Open file explorer
-      let input = document.createElement("input");
-      input.setAttribute("type", "file");
-      input.click();
-  
-      input.addEventListener("change", (e) => {
-          let file = input.files[0];
-          let url = URL.createObjectURL(file);
-  
-          let stickyTemplateHTML = `
-          <div class="header-cont">
-              <div class="minimize"></div>
-              <div class="remove"></div>
-          </div>
-          <div class="note-cont">
-              <img src="${url}"/>
-          </div>
-          `;
-          createSticky(stickyTemplateHTML);
-      })
+    // Open file explorer
+    let input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.click();
+
+    input.addEventListener("change", (e) => {
+        let file = input.files[0];
+        let url = URL.createObjectURL(file);
+
+        let stickyTemplateHTML = `
+        <div class="header-cont">
+            <div class="minimize"></div>
+            <div class="remove"></div>
+        </div>
+        <div class="note-cont">
+            <img src="${url}"/>
+        </div>
+        `;
+        createSticky(stickyTemplateHTML);
+    })
 })
 
-function createSticky(stickyTemplateHTML) {
+sticky.addEventListener("click", (e) => {
     let stickyTemplateHTML = `
     <div class="header-cont">
         <div class="minimize"></div>
@@ -84,18 +86,26 @@ function createSticky(stickyTemplateHTML) {
     `;
 
     createSticky(stickyTemplateHTML);
-}
-sticky.addEventListener("click", (e) => {
-    let stickyTemplateHTML =`
-    <div class="header-cont">
-        <div class="minimize"></div>
-        <div class="remove"></div>
-    </div>
-    <div class="note-cont">
-        <textarea></textarea>
-    </div>
-    `;
 })
+
+function createSticky(stickyTemplateHTML) {
+    let stickyCont = document.createElement("div");
+    stickyCont.setAttribute("class", "sticky-cont");
+    stickyCont.innerHTML = stickyTemplateHTML;
+    document.body.appendChild(stickyCont);
+
+    let minimize = stickyCont.querySelector(".minimize");
+    let remove = stickyCont.querySelector(".remove");
+    noteActions(minimize, remove, stickyCont);
+
+    stickyCont.onmousedown = function (event) {
+        dragAndDrop(stickyCont, event);
+    };
+
+    stickyCont.ondragstart = function () {
+        return false;
+    };
+}
 
 function noteActions(minimize, remove, stickyCont) {
     remove.addEventListener("click", (e) => {
@@ -108,6 +118,7 @@ function noteActions(minimize, remove, stickyCont) {
         else noteCont.style.display = "none";
     })
 }
+
 function dragAndDrop(element, event) {
     let shiftX = event.clientX - element.getBoundingClientRect().left;
     let shiftY = event.clientY - element.getBoundingClientRect().top;
